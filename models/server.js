71fce275b79a914e5e6d61d46daf88a5a1102aa5
Julian Carrasco <jecc_8212@gmail.com>
2023-08-12 22:28:05 -0400

    Fin seccio 9

--- models/server.js
+++ models/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 class Server {
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
+
+        //Conectar a bd
+        this.conectarDB();
 
         //Middlewares
         this.middlwwares();
@@ -15,6 +19,10 @@ class Server {
 
         this.routes();
     }
+
+ async conectarDB(){
+    await dbConnection();
+ }
 
 middlwwares(){
 
